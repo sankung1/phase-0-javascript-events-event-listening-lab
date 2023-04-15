2fc914fe822cc6cc41a4b13de0ce31c0f53731ff
Sankung Sillah <ssillah306@gmail.com>
2023-04-14 23:34:56 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.querySelector('#button');
+
+function clickedAlert(){
+    alert('I was clicked');
+}
+
 function addingEventListener() {
+    input.addEventListener('click', clickedAlert);
 }
